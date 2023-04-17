340cb94c88cf3473ddcee474c7574b60f5638dd1
Alma <Alotte@users.noreply.github.com>
2023-04-17 18:27:24 +0200

    What is GPT3 & Additional styling

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-function Feature() {
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div className ="gpt3__features-container__feature-bar"/>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
